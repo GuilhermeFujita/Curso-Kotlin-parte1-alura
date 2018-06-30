0558f2abe2a24ac4bff7a1421d89c272fa12a27b
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-06-30 16:10:11 -0300

    Implemented adapter getItem

--- financas-kotlin/app/src/main/java/fujita/guilherme/financask/ui/adapter/ListaTransacoesAdapter.kt
+++ financas-kotlin/app/src/main/java/fujita/guilherme/financask/ui/adapter/ListaTransacoesAdapter.kt
@@ -4,14 +4,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 
-class ListaTransacoesAdapter : BaseAdapter() {
-    
+class ListaTransacoesAdapter(transacoes: List<String>) : BaseAdapter() {
+
+    private val transacoes = transacoes;
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getItem(position: Int): Any {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getItem(posicao: Int): String {
+        return transacoes[posicao]
     }
 
     override fun getItemId(position: Int): Long {
