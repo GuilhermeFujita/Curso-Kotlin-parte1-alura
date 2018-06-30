a9586d26644f0aad08c1ac44dd5e8c746e7735e5
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-06-30 16:15:12 -0300

    Implemented adapter getView - test

--- financas-kotlin/app/src/main/java/fujita/guilherme/financask/ui/adapter/ListaTransacoesAdapter.kt
+++ financas-kotlin/app/src/main/java/fujita/guilherme/financask/ui/adapter/ListaTransacoesAdapter.kt
@@ -1,14 +1,20 @@
 package fujita.guilherme.financask.ui.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import fujita.guilherme.financask.R
 
-class ListaTransacoesAdapter(transacoes: List<String>) : BaseAdapter() {
+class ListaTransacoesAdapter(transacoes: List<String>,
+                             context: Context) : BaseAdapter() {
 
-    private val transacoes = transacoes;
+    private val transacoes = transacoes
+    private  val context = context
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+    override fun getView(position: Int, view: View?, parent: ViewGroup?): View {
+        return LayoutInflater.from(context).inflate(R.layout.transacao_item, parent, false)
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
