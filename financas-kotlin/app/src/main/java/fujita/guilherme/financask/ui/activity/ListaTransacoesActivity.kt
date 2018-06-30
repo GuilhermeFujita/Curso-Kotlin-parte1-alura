252427e3629f2699df2425892092bfea2319ffef
GuilhermeFujita <fujitaguilherme@gmail.com>
2018-06-30 15:41:47 -0300

    Creating list and using default adapter

--- financas-kotlin/app/src/main/java/fujita/guilherme/financask/ui/activity/ListaTransacoesActivity.kt
+++ financas-kotlin/app/src/main/java/fujita/guilherme/financask/ui/activity/ListaTransacoesActivity.kt
@@ -2,12 +2,21 @@ package fujita.guilherme.financask.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
 import fujita.guilherme.financask.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes: List<String> = listOf("Comida - R$20,00 ", "Economia - R$100,00")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+        
+        //Seta o adapter na lista
+        lista_transacoes_listview.setAdapter(arrayAdapter)
     }
 }
